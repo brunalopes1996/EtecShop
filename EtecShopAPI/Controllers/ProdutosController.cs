e1c92854a57ee2a4a648741b6f1798c47cf76d2d
brunalopes1996 <bruuna.oliveiraa1@gmail.com>
2024-11-29 22:26:05 -0300

    Terminando a criação da API

--- EtecShopAPI/Controllers/ProdutosController.cs
+++ EtecShopAPI/Controllers/ProdutosController.cs
@@ -5,9 +5,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EtecShopAPI.Controllers;
 
-    [ApiController]
-    [Route("api/produtos")]
-    public class ProdutosController(AppDbContext db) : ControllerBase
+[ApiController]
+[Route("api/produtos")]
+public class ProdutosController(AppDbContext db) : ControllerBase
+{  
+    private readonly AppDbContext _db = db;
+
+    [HttpGet]
+    [ProducesResponseType(200)]
+    public async Task<IActionResult> Get()
     {
-        
+        List<Produto> produtos = await _db.Produtos.Include(p => p.Categoria).ToListAsync();
+        return Ok(produtos);
     }
+
+}
